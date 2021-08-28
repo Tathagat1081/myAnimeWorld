a158e5c6f1a91ae5f47e5d5bae39c31a86d6b2f1
Tathagat <tathagat1081@gmail.com>
2021-08-28 21:11:37 +0530

    first page

--- src/main/java/com/project/rest/AnimeImageController.java
+++ src/main/java/com/project/rest/AnimeImageController.java
@@ -1,14 +1,16 @@
 package com.project.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins= "http://localhost:3000")
 @RestController
 public class AnimeImageController
     {
         @RequestMapping("/hello")
         public String sayHi()
         {
-            return "Hi";
+            return "Hi,welcome to my Anime world.I am Tathagat,creator of this page.I am a third year Engineering student pursuing BTech in Computer Science from N.I.T Andhra Pradesh.My hobbies are singing,watching Anime. ";
         }
 }
